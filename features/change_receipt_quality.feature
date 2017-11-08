a2b61c0ae6c61b1997f669987eef72eed06ef3d3
Luke Hemingway <lukehemingway66@gmail.com>
2017-11-08 15:12:56 +0000

    Added change receipt feature

---
+++ features/change_receipt_quality.feature
@@ -0,0 +1,6 @@
+Feature: I am able to change the receipt quality
+
+	Given I am on the homepage
+	When I click on the settings button
+	And Click on receipt quality
+	Then I can change the receipt quality 
---
+++ features/step_definitions/change_receipt_quality_steps.rb
@@ -0,0 +1,8 @@
+# Given(/^I am on the homepage$/)do
+# end
+# When(/^I click on the settings button$/)do
+# end 
+# And(/^Click on receipt quality$/)do 
+# end
+# Then(/^I can change the receipt quality$/)do
+# end
