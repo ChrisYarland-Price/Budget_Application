e45a3acd2eb8f1ec231e250e0a64c7fbe100fcee
Chris Yarland-Price <chris.yarland@protonmail.com>
2017-11-08 14:45:32 +0000

    Before hook Created

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,6 +2,13 @@ Before do
   $driver.start_driver
 end
 
+Before('@budget_created') do
+  main_page.new_budget.click
+  budget_page.add_budget.click
+  budget_page.budget_name.type("Sparta Global Budget")
+  budget_page.value_edit.type("10000")
+    budget_page.save_button.click
+end
 
 After do
   $driver.driver_quit
