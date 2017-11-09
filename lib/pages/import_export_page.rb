5cd9566c487b7775ef2ff291231efc92501aba2d
Ginxty <james.oneill96@gmail.com>
2017-11-09 12:27:02 +0000

    send help on export

--- lib/pages/import_export_page.rb
+++ lib/pages/import_export_page.rb
@@ -1,7 +1,9 @@
 class ExportPage
   @@import_export_button = 'protect.budgetwatch:id/action_import_export'
 
-  @@export_field =  'android:id/text1'
+  @@export_field =  '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.Spinner/android.widget.TextView'
+
+  @@android_file_allow = 'com.android.packageinstaller:id/permission_allow_button'
 
   @@success_field =  'protect.budgetwatch:id/title_template'
 
@@ -11,7 +13,12 @@ class ExportPage
     $driver.find_element(:id, @@import_export_button)
   end
 
+  def file_allow
+    $driver.find_element(:id, @@android_file_allow)
+  end
+  
   def export_field
+    
     $driver.find_element(:id, @@export_field)
   end
 
